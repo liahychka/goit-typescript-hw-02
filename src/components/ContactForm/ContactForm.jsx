591c64a3de567118aac7545774564e0000b5b4a0
liahychka <157300297+liahychka@users.noreply.github.com>
2024-08-13 11:41:41 +0200

    .

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from "yup";
 import css from "./ContactForm.module.css"
 
@@ -9,10 +9,10 @@ const ProfileValidationSchema = Yup.object().shape({
   tel: Yup.string().required("Number is required").min(3, "Number must be at least 3 characters").max(50, "Number can't exceed 50 characters"),
 });
 
-const ContactForm = ({ onAddProfile }) => {
+const ContactForm = ({ onSubmit }) => {
   
       const handleSubmit = (values, { resetForm }) => {
-      onAddProfile({
+      onSubmit({
         id: Date.now(),
         name: values.username,
         number: values.tel,
@@ -30,11 +30,13 @@ const ContactForm = ({ onAddProfile }) => {
               <label className={css.capitalForm1}>
                   <span>Name</span>
                   <Field type="text" name="username" />     
-              </label>
+        </label>
+        <ErrorMessage name="username" component="div" className={css.error} />
               <label className={css.capitalForm2}>
                 <span>Number</span>
                <Field type="tel" name="tel"/>   
-              </label>
+        </label>
+      <ErrorMessage name="username" component="div" className={css.error} />
 			<button type="submit" className={css.btnAdd} >Add contact</button>
 		</Form>
     </Formik>
